91c03e5cd7603dfcf5c36880668217e5e33c89e6
tesla7 <77625961@qq.com>
2020-01-06 10:38:35 +0800

    更新 phabricator

---
+++ phabricator/phabricator.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+`docker exec phabricator_phabricator_1 bash /opt/bitnami/phabricator/bin/config set phabricator.base-uri 'http://matrix.phabricator.com'`
+`docker-compose restart`
